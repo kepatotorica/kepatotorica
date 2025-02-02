a7aa3e5e884ab578c2e3b1b09c78cf8c64b537da
kepa totorica <kepatoto@gmail.com>
2025-02-02 11:22:13 -0700

    main: onPress vs onClick

--- app/wordle/WordleGuesser/WordleGuesser.tsx
+++ app/wordle/WordleGuesser/WordleGuesser.tsx
@@ -1,14 +1,14 @@
-"use client";
-import React from "react";
-import "./WordleGuesser.css";
-import { Button } from "@nextui-org/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
+"use client"
+import React from "react"
+import "./WordleGuesser.css"
+import { Button } from "@nextui-org/button"
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react"
 
-import { deepCopy } from "../../../Utilities/deepCopy";
+import { deepCopy } from "../../../Utilities/deepCopy"
 
-import { Color, Position } from "./Position";
-import { trimWords, words } from "./WordProcessor";
-import LetterSlot from "./LetterSlot";
+import { Color, Position } from "./Position"
+import { trimWords, words } from "./WordProcessor"
+import LetterSlot from "./LetterSlot"
 
 type MyState = {
   gridList: Position[][];
@@ -19,52 +19,52 @@ type MyState = {
 const letterSlotRefs: React.RefObject<HTMLInputElement>[][] = Array.from(
   { length: 6 },
   () => Array(5).fill(React.createRef<HTMLInputElement>()),
-);
+)
 
 function createStartingGrid(): Position[][] {
-  let rows: Position[][] = new Array(6);
+  let rows: Position[][] = new Array(6)
 
   for (let index = 0; index < rows.length; index++) {
-    let columnPosition: number = 0;
+    let columnPosition: number = 0
 
     rows[index] = Array(5)
       .fill(new Position("", -1, 0))
-      .map((el: Position) => ({ ...el, Index: columnPosition++ }));
+      .map((el: Position) => ({ ...el, Index: columnPosition++ }))
   }
 
   rows.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
-      letterSlotRefs[rowIndex] = letterSlotRefs[rowIndex] || [];
-      letterSlotRefs[rowIndex][colIndex] = React.createRef<HTMLInputElement>();
-    });
-  });
+      letterSlotRefs[rowIndex] = letterSlotRefs[rowIndex] || []
+      letterSlotRefs[rowIndex][colIndex] = React.createRef<HTMLInputElement>()
+    })
+  })
 
-  return rows;
+  return rows
 }
 
 const initialState: MyState = {
   gridList: createStartingGrid(),
   wordList: [],
   anchorEl: null,
-};
+}
 
 class WordleGuesser extends React.Component<any, MyState> {
   constructor(props: any) {
-    super(props);
+    super(props)
     this.state = {
       ...initialState,
-    };
+    }
   }
 
   filterWords() {
-    let calculatedWordList = words;
-    let greenLetters: Position[] = [];
+    let calculatedWordList = words
+    let greenLetters: Position[] = []
 
     this.state.gridList.forEach((row) => {
       row.forEach((letter) => {
         let overlappingLetters = greenLetters.filter(
           (greenLetter) => greenLetter.Letter === letter.Letter,
-        );
+        )
 
         if (overlappingLetters.length > 0) {
           if (letter.Color !== Color.Grey) {
@@ -72,61 +72,62 @@ class WordleGuesser extends React.Component<any, MyState> {
             //TODOASDF: if we have a yellow, and a green, then we need to only find words where there are both the green and another occurance of the same
             // letter in the word right now the way I have it written would mean making some changes to how the processing gets done. I am just a little too
             // lazy right now to fix that
-            calculatedWordList = trimWords(letter, calculatedWordList);
+            calculatedWordList = trimWords(letter, calculatedWordList)
           }
         } else {
-          calculatedWordList = trimWords(letter, calculatedWordList);
+          calculatedWordList = trimWords(letter, calculatedWordList)
         }
 
         if (letter.Color === Color.Green) {
-          greenLetters = greenLetters.concat(letter);
+          greenLetters = greenLetters.concat(letter)
         }
-      });
-    });
+      })
+    })
 
-    this.setState({ wordList: calculatedWordList });
+    debugger
+    this.setState({ wordList: calculatedWordList })
   }
 
   updateColorForPosition(row: number, col: number): void {
-    let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList);
-    const selectedLetter = copiedGridOfLetters[row][col];
+    let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList)
+    const selectedLetter = copiedGridOfLetters[row][col]
 
     if (selectedLetter.Letter !== "") {
       if (++selectedLetter.Color > Color.Green) {
-        selectedLetter.Color = Color.Grey;
+        selectedLetter.Color = Color.Grey
       }
       this.setState(
         { gridList: deepCopy(copiedGridOfLetters) },
         this.filterWords,
-      );
+      )
     }
   }
 
   inputChanged(row: number, col: number, activeInput: string) {
-    this.updateColorForPosition(row, col);
-    let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList);
-    const currentCell = copiedGridOfLetters[row][col];
+    this.updateColorForPosition(row, col)
+    let copiedGridOfLetters: Position[][] = deepCopy(this.state.gridList)
+    const currentCell = copiedGridOfLetters[row][col]
 
     if (activeInput === "") {
-      currentCell.Letter = "";
-      currentCell.Color = Color.Nothing;
+      currentCell.Letter = ""
+      currentCell.Color = Color.Nothing
     } else {
-      currentCell.Letter = activeInput;
+      currentCell.Letter = activeInput
 
       let setTheInitialColorWhenWeAddALetter =
-        currentCell.Color === Color.Nothing;
+        currentCell.Color === Color.Nothing
 
       if (setTheInitialColorWhenWeAddALetter) {
-        currentCell.Color = Color.Grey;
+        currentCell.Color = Color.Grey
       }
 
-      const nextRowIndex = row;
-      const nextColIndex = col + 1;
+      const nextRowIndex = row
+      const nextColIndex = col + 1
 
       if (nextColIndex < this.state.gridList[nextRowIndex].length) {
-        letterSlotRefs[nextRowIndex][nextColIndex].current?.focus();
+        letterSlotRefs[nextRowIndex][nextColIndex].current?.focus()
       } else if (nextRowIndex < this.state.gridList.length - 1) {
-        letterSlotRefs[nextRowIndex + 1][0].current?.focus();
+        letterSlotRefs[nextRowIndex + 1][0].current?.focus()
       }
     }
 
@@ -134,28 +135,28 @@ class WordleGuesser extends React.Component<any, MyState> {
       this.setState(
         { gridList: deepCopy(copiedGridOfLetters) },
         this.filterWords,
-      );
+      )
     }
   }
 
   handleKeyPressed(row: number, col: number, keyPressed: string) {
-    const currentCell = this.state.gridList[row][col];
+    const currentCell = this.state.gridList[row][col]
     const shouldNavBack =
-      keyPressed === "Backspace" && currentCell.Letter === "";
+      keyPressed === "Backspace" && currentCell.Letter === ""
 
     if (shouldNavBack) {
-      let prevRowIndex = row;
-      let prevColIndex = col - 1;
+      let prevRowIndex = row
+      let prevColIndex = col - 1
 
       if (prevColIndex < 0) {
-        prevColIndex = this.state.gridList[0].length - 1;
-        prevRowIndex--;
+        prevColIndex = this.state.gridList[0].length - 1
+        prevRowIndex--
       }
       if (prevRowIndex < 0) {
-        prevRowIndex = 0;
-        prevColIndex = 0;
+        prevRowIndex = 0
+        prevColIndex = 0
       }
-      letterSlotRefs[prevRowIndex][prevColIndex].current?.focus();
+      letterSlotRefs[prevRowIndex][prevColIndex].current?.focus()
     }
   }
 
@@ -184,7 +185,7 @@ class WordleGuesser extends React.Component<any, MyState> {
                     />
                   ))}
                 </div>
-              );
+              )
             })}
           </div>
           <div className="button-container">
@@ -214,8 +215,8 @@ class WordleGuesser extends React.Component<any, MyState> {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default WordleGuesser;
+export default WordleGuesser
