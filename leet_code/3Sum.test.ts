96f1a9cc884de234d9e15f98fc6e472047cc43b1
kepatoto <kepatoto@gmail.com>
2025-04-01 21:15:15 -0600

    main: answered question

--- leet_code/3Sum.test.ts
+++ leet_code/3Sum.test.ts
@@ -6,144 +6,76 @@
 describe("testing template", () => {
   //Binary search recursive tests
   test("binaray exists 1", () => {
-    expect(exists([1,2,3,4,5,6], 0, 5, 3)).toBe(true)
+    expect(existsBinary([1, 2, 3, 4, 5, 6], 0, 5, 3)).toBe(true)
   })
 
   test("binaray exists 2", () => {
-    expect(exists([1,2,3,4,5,6], 0, 5, 10)).toBe(false)
+    expect(existsBinary([1, 2, 3, 4, 5, 6], 0, 5, 10)).toBe(false)
   })
 
   test("binaray exists works 3", () => {
-    expect(exists([1,2,3,4,5,6], 0, 5, 0)).toBe(false)
+    expect(existsBinary([1, 2, 3, 4, 5, 6], 0, 5, 0)).toBe(false)
   })
 
   test("binaray exists works 4", () => {
-    expect(exists([0], 0, 0, 0)).toBe(true)
+    expect(existsBinary([0], 0, 0, 0)).toBe(true)
   })
 
   test("binaray exists works 5", () => {
-    expect(exists([1], 0, 0, 0)).toBe(false)
+    expect(existsBinary([1], 0, 0, 0)).toBe(false)
   })
-
 
   //General test cases
   test("case 1", () => {
-    expect(threeSum([-1,0,1,2,-1,-4])).toBe([[-1,-1,2],[-1,0,1]])
+    expect(threeSum([-4,-1,-1,0,1,2])).toStrictEqual([
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ])
   })
 })
 
 function threeSum(nums: number[]): number[][] {
   //Sort the original array
-  nums = nums.sort()
+  nums = nums.sort((a, b) => a - b)
 
-  //have a pointer on the left, and one on the right
-  let leftIndex = 0
-  let rightIndex = nums.length
+  let validEntriesMap: Map<string,number[]> = new Map<string, number[]>()
 
-  // let validEntries = []
-  // for(let i = 0; i < nums.length / 2; i++){
-  //   // if the right minus the left is zero, break, unless the left item is negative, as 0 is a valid value
-  //   let leftValue = nums[leftIndex]
-  //   let rightValue = nums[rightIndex]
-  //   let requiredMidValue = leftValue - rightValue
+  for (let lIndex = 0; lIndex < nums.length; lIndex++) {
+    let rIndex = nums.length - 1
+    while (lIndex + 1 < rIndex) { //+2 because we only look at three items, if we will go below that stop
+      let lValue = nums[lIndex]
+      let rValue = nums[rIndex]
 
-  //   if(exists(nums, leftIndex, rightIndex, requiredMidValue)){
-  //     validEntries.push([leftValue, requiredMidValue, rightValue])
-  //   }
+      let mid = -1 * (rValue + lValue)
+      if (existsBinary(nums, lIndex+1, rIndex-1, mid)) {
+        if(mid == -0){
+          mid = 0
+        }
+        validEntriesMap.set(`${lValue}, ${mid}, ${rValue}`, [lValue, mid, rValue])
+      }
 
-  //   // leftValue = nums[leftIndex+1]
-  //   // rightValue = nums[rightIndex]
-  //   // requiredMidValue = leftValue - rightValue
-  //   // if(exists(nums, leftIndex+1, rightIndex, requiredMidValue)){
-  //   //   validEntries.push([leftValue, requiredMidValue, rightValue])
-  //   // }
-
-  //   // leftValue = nums[leftIndex]
-  //   // rightValue = nums[rightIndex-1]
-  //   // requiredMidValue = leftValue - rightValue
-  //   // if(exists(nums, leftIndex, rightIndex-1, requiredMidValue)){
-  //   //   validEntries.push([leftValue, requiredMidValue, rightValue])
-  //   // }
-
-  //   // We can't just move the pointer, after checking the current values, we need to do, L+1 -> R and L -> R-1 then we need to move both pointers to L+1 -> R-1
-
-
-  //   // We need to skip x number when changing the index, until we don't find the same number
-  //   // let diff = 0
-  //   // if(leftMovedLast){
-  //   //   --rightIndex
-  //   //   // diff = nums[rightIndex] - rightValue
-  //   //   // while(diff == 0 && leftIndex !== rightIndex){
-  //   //   //   --rightIndex
-  //   //   //   diff = nums[rightIndex] - rightValue
-  //   //   //   ++i
-  //   //   // }
-  //   //   leftMovedLast = false
-  //   // }else{
-  //   //   ++leftIndex
-  //   //   // diff = nums[leftIndex] - rightValue
-  //   //   // while(diff == 0 && leftIndex !== rightIndex){
-  //   //   //   ++leftIndex
-  //   //   //   diff = nums[leftIndex] - rightValue
-  //   //   //   ++i
-  //   //   // }
-  //   //   leftMovedLast = true
-  //   // }
-  // }
-
-
-  const arrays = getAllMatches(nums, leftIndex, rightIndex)
-  return arrays
-};
-
-function getAllMatches(nums: number[], leftIndex: number, rightIndex: number){
-  let validEntries = []
-
-  if(leftIndex == rightIndex) return []
-
-  let leftValue = nums[leftIndex]
-  let rightValue = nums[rightIndex]
-  let requiredMidValue = leftValue - rightValue
-
-  if(exists(nums, leftIndex, rightIndex, requiredMidValue)){
-    validEntries.push([leftValue, requiredMidValue, rightValue])
+      --rIndex
+    }
   }
 
-  leftValue = nums[leftIndex+1]
-  rightValue = nums[rightIndex]
-  requiredMidValue = leftValue - rightValue
-  if(exists(nums, leftIndex+1, rightIndex, requiredMidValue)){
-    validEntries.push(getAllMatches(nums, leftIndex+1, rightIndex))
-  }
-
-  leftValue = nums[leftIndex]
-  rightValue = nums[rightIndex-1]
-  requiredMidValue = leftValue - rightValue
-  if(exists(nums, leftIndex, rightIndex-1, requiredMidValue)){
-    validEntries.push(getAllMatches(nums, leftIndex, rightIndex-1))
-  }
-  //if l == r return
-  //l+1,r append
-  //l,r-1 append
-  //l,r append
-
-  return validEntries
+  return Array.from(validEntriesMap.values())
 }
 
-function exists(nums: number[], startIndex: number, endIndex:number, valueToFind: number): boolean {
+
+function existsBinary(nums: number[], startIndex: number, endIndex: number, valueToFind: number): boolean {
   let midIndex = Math.ceil((startIndex + endIndex) / 2)
   let midValue = nums[midIndex]
 
-  if(startIndex == endIndex){
+  if (startIndex == endIndex) {
     return valueToFind == midValue
   }
 
-  if(midValue > valueToFind){
-    return exists(nums, startIndex, midIndex - 1, valueToFind)
+  if (midValue > valueToFind) {
+    return existsBinary(nums, startIndex, midIndex - 1, valueToFind)
   }
 
-  if(midValue <= valueToFind){
-    return exists(nums, midIndex, endIndex , valueToFind)
+  if (midValue <= valueToFind) {
+    return existsBinary(nums, midIndex, endIndex, valueToFind)
   }
 
   return false
